75da7175f6e68ba5ce65e2fc9b8c8d8f04f6817d
Albenus Pieter <146658653+albenusmurmu@users.noreply.github.com>
2025-05-30 00:06:15 +0530

    Update Contact.js

--- src/landingPage/contact/Contact.js
+++ src/landingPage/contact/Contact.js
@@ -37,7 +37,7 @@ function Contact() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/contact', {
+            const response = await fetch('https://sonycom-backend.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
